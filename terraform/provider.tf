1090f904ff56b13300632674f727f48417806779
Luca Pasquali <lpasqua@gmail.com>
2019-11-02 19:40:27 +1100

    mock login config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,10 @@
 provider "aws" {
-  # ... potentially other provider configuration ...
+  access_key                  = "mock_access_key"
+  region                      = "us-east-1"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
     kinesis  = "http://localhost:4568"
