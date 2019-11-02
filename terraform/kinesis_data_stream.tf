8bc5aab7f8d1b3df016b3ab891ebd3ed53ff0d0c
Luca Pasquali <lpasqua@gmail.com>
2019-11-02 19:50:02 +1100

    data stream config

--- terraform/kinesis_data_stream.tf
+++ terraform/kinesis_data_stream.tf
@@ -3,10 +3,10 @@ resource "aws_kinesis_stream" "test_stream" {
   shard_count      = 1
   retention_period = 48
 
-  shard_level_metrics = [
-    "IncomingBytes",
-    "OutgoingBytes",
-  ]
+#  shard_level_metrics = [
+#    "IncomingBytes",
+#    "OutgoingBytes",
+#  ]
 
   tags = {
     Environment = "test"
